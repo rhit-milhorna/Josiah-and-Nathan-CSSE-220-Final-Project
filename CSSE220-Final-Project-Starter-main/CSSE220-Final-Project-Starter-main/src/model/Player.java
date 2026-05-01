7b046cc365d19a4763ddb97613d635e4c36557d9
rhit-laymanjn <laymanjn@rose-hulman.edu>
2026-05-01 15:20:46 -0400

    Update Player.java

--- CSSE220-Final-Project-Starter-main/CSSE220-Final-Project-Starter-main/src/model/Player.java
+++ CSSE220-Final-Project-Starter-main/CSSE220-Final-Project-Starter-main/src/model/Player.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
 public class Player {
+	
+	private int x, y, width, height;
+	private int startx, starty;
+	BufferedImage sprite;
+	
+	public Player(int x, int y, int width, int height) {
+		this.x = x;
+		this.
+	}
+	
 
 }
