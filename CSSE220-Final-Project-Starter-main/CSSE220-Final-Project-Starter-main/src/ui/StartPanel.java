f5803f134580c6dee49e9f1dc35cd618b6d35ae5
rhit-laymanjn <laymanjn@rose-hulman.edu>
2026-05-20 11:34:38 -0400

    Added start screen

---
+++ CSSE220-Final-Project-Starter-main/CSSE220-Final-Project-Starter-main/src/ui/StartPanel.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class StartPanel extends JPanel{
+	
+	private JLabel title = new JLabel("Main Menu");
+	public JButton start = new JButton("Start Game");
+	
+	public StartPanel() {
+		
+		this.setLayout(new BorderLayout());
+		add(title, BorderLayout.CENTER);
+		add(start, BorderLayout.SOUTH);
+	}
+
+}
